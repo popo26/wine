f2ec2e5767a1d822b6d2e103893f9ae3d4d3fe79
popo26 <mynameispopo26@gmail.com>
2022-11-05 10:29:11 +1300

    added UserSchema - Not working

--- server.js
+++ server.js
@@ -1,29 +1,72 @@
 require('dotenv').config();
-// const apiKey = process.env.API_KEY;
+const secret = process.env.SECRET;
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs")
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const _ = require("lodash");
+
+// Authentication1-position important
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const app = express();
+
+app.set("view engine", 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+// Authentication2 - position important
+// app.use(session({
+//     secret: secret;
+//     resave: false,
+//     saveUninitialized: false
+// }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 
 // +++++++++++++++create db if it exists, it will just use it+++++++++++++++
 mongoose.connect("mongodb://localhost:27017/winesDB")
 
+
 // +++++++++++++++create new schema and add mongoose's built-in validation+++++++++++++++
 //https://mongoosejs.com/docs/validation.html
 const winesSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Wine name is missing!"]
-    },
+    // name: {
+    //     type: String,
+    //     required: [true, "Wine name is missing!"]
+    // },
+    name: String,
     type: String,
     comment: String,
     price: Number,
+
 });
+
+const usersSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email is required."]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required."]
+    },
+   
+});
+
+
+// Authentication3 - position important
+
 
 //+++++++++++++++create model+++++++++++++++
 const Wine = mongoose.model("Wine", winesSchema);
+const User = mongoose.model("User", usersSchema);
 
 //+++++++++++++++create wine document - single creation+++++++++++++++
 // const wine = new Wine ({
@@ -109,27 +152,69 @@ const Wine = mongoose.model("Wine", winesSchema);
 // });
 
 
-
-const app = express();
-
-app.set("view engine", 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
-
 const dislikes = [];
 
 app.get("/", function(req, res){
+    res.render("home");
+});
+
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password,
+    });
+
+    console.log(req.body.username);
+    console.log(req.body.password);
+
+    newUser.save(function(err){
+        if (err){
+            console.log(err);
+        } else {
+            res.render('login');
+        }
+    });
+});
+
+app.get("/login", function(req,res) {
+    res.render("login");
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password){
+                    res.render("/wines");
+                }
+            }
+        }
+    });
+});
+
+app.get("/wines", function(req, res){
+
     Wine.find({}, function(err, foundWines){
         if(err){
             console.log(err);
         } else {
-            res.render("home", {dislikeList: foundWines});
+            res.render("wines", {dislikeList: foundWines});
         }  
     });  
 });
 
-app.post("/", function(req, res){
-    const wineName = req.body.wineName;
+app.post("/wines", function(req, res){
+    const wineName = _.capitalize(req.body.wineName);
     const wineType = req.body.wineType;
     const wineComment = req.body.wineComment;
     const winePrice = req.body.winePrice;
@@ -143,7 +228,7 @@ app.post("/", function(req, res){
 
     wine.save()
 
-    res.redirect("/");
+    res.redirect("/wines");
 })
 
 app.post("/delete", function(req, res){
@@ -152,10 +237,12 @@ app.post("/delete", function(req, res){
     Wine.findByIdAndRemove(deleteWineId, function(err){
         if (!err) {
             console.log("Removed selected wine.");
-            res.redirect("/");
+            res.redirect("/wines");
         }
     });
 });
+
+
 
 app.listen(3000, function(){
     console.log ("Server started on port 3000")
